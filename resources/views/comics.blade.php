7a0e0d3ecd692600613b6972911c52b76bd4c125
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-06-08 21:47:00 +0200

    cards and scss

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,18 +4,22 @@
 
 @section('content')
 
-<section class="container">
+<section class="container flex-center">
+    
     <div class="cards">
 
         @foreach($cards as $card)
             <div class="card">
                 <img src=" {{ $card['image'] }} " alt=" {{ $card['title']}} ">
+                <span class="title"> {{ $card['title'] }}</span> 
             </div>
         @endforeach
 
+        
+
     </div>
 
-    
+    <a class="btn flex-center" href="#">load more</a>
 
 </section>
 
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'News | Comics')
 
 @section('content')
+<section class="container">
 
-<h1>News</h1>
-
+    <h1>News</h1>
+</section>
 @endsection
